fdec16d463cc590cd012565b414c10bc3b24e22a
chiemelaumeh <chiemelaumeh1@gmail.com>
2024-03-01 03:24:58 -0600

    new

---
+++ server/server.js
@@ -0,0 +1,81 @@
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+import uploadRouter from "./routes/uploadRoutes.js";
+
+
+dotenv.config();
+mongoose.set("strictQuery", false);
+const connectionString = process.env.MONGODB_URI
+
+ const connectDb = async() => {
+  try {
+    await mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+  
+    });
+    const db = mongoose.connection;
+    db.on("error", console.log);
+    
+  } catch (error) {
+    console.error(error.message)
+  }
+}
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/keys/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
+app.get("/api/keys/google", (req, res) => {
+  res.send({ key: process.env.GOOGLE_API_KEY || "" });
+});
+
+
+
+app.use("/api/upload", uploadRouter);
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
+
+app.use((err, req, res, next)=> {
+  const errorStatus = err.status || 500
+  const errorMessage = err.message || "Something went wrong"
+  return res.status(errorStatus).json({
+    successful: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  })
+})
+
+
+const __dirname = path.resolve();
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (req, res) =>
+  // res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
+  res.json(apiinfo)
+
+);
+connectDb()
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`...Listeniing on port ${port}`);
+});
