d40ef9b33f3daafe690282c6d9c349c1f37cf98f
Chiemela Umeh <chiemelaumeh@Chiemelas-MacBook-Pro.local>
2024-04-03 01:46:46 -0500

    new

--- server/db.js
+++ server/db.js
@@ -2,19 +2,14 @@ import dotenv from 'dotenv';
 import mysql from 'mysql2';
 dotenv.config();
 
+export const pool = mysql.createPool({
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DATABASE,
 
-
-
-// export const pool = mysql.createPool({
-//     user: process.env.DATABASE_USER,
-//     host: process.env.DATABASE_HOST,
-//     password: process.env.DATABASE_PASSWORD,
-//     database: process.env.DATABASE_DATABASE,
-//     ssl: process.env.DATABASE_URL ?{ rejectUnauthorized: false } : false
-//   });
+  }).promise();
   
 
 
-const DATABASE_URL = process.env.DATABASE_URL;
-export const pool = mysql.createPool(DATABASE_URL).promise();
-// console.log(pool)
+
